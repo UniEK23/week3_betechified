19339fec0479b18a6d735ecbb5b747dc21f3a711
UniEK23 <anaikotekan@gmail.com>
2026-07-31 08:55:01 +0100

    Added cors dependency and modifications for deployment

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
 app.use(express.json()); // Parse JSON bodies
+app.use(cors('*'));
 
 let todos = [
   { id: 1, task: 'Learn Node.js', completed: false },
@@ -73,5 +79,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Server error!' });
 });
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
